7c217557ce1f32b559c4d16867be665126c3dc5d
EdS <Sousaa.dev@gmail.com.br>
2025-09-29 18:11:14 -0300

    Criação de novas paginas html para categoria e reestruturação do model e controller

---
+++ app/controllers/categorias_controller.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, render_template, redirect, url_for
+from app.models.produto import  get_categorias, insert_categoria
+
+categoria_bp = Blueprint("categoria", __name__)
+
+    
+@categoria_bp.route("/categoria/lista", methods=["GET"])
+def listar_categorias():
+    categorias = get_categorias()
+    return render_template('categoria.html', categorias= categorias)
+
+@categoria_bp.route("/cadastrar", methods=["POST"])
+def criar_categoria():
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+
+    insert_categoria(nome, descricao)
+    return redirect(url_for('categoria.listar_categorias'))
+
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.controllers.produtos_controller import produto_bp
 from app.controllers.compra_controller import compra_bp
 from app.controllers.fornecedor_controller import fornecedor_bp
 from app.controllers.venda_controller import venda_bp
+from app.controllers.categorias_controller import categoria_bp
 
 app = Flask(__name__, static_folder="app/static")
 app.secret_key = Config.SECRET_KEY
@@ -15,6 +16,7 @@ app.register_blueprint(produto_bp)
 app.register_blueprint(compra_bp)
 app.register_blueprint(venda_bp)
 app.register_blueprint(fornecedor_bp)
+app.register_blueprint(categoria_bp)
 
 if __name__ == "__main__":
   app.run(debug=True)
