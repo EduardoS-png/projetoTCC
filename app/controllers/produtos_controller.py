ccbb9ecd7547324083c1b0b3db8f5b9ea58c92c5
EdS <Sousaa.dev@gmail.com.br>
2025-10-01 09:05:20 -0300

    Correção no model e controller de produto

--- app/controllers/produtos_controller.py
+++ app/controllers/produtos_controller.py
@@ -1,67 +1,56 @@
 from flask import Blueprint, request, render_template, redirect, jsonify, url_for
-from app.models.produto import get_produtos, get_produtos_id, insert_produtos, update_produto, inative_produto, reative_produto
+from app.models.produto import *
 
 produto_bp = Blueprint("produto", __name__)
 
 @produto_bp.route("/produto/lista", methods=["GET"])
 def listar_produtos():
-    categoria_id = request.args.get("categoria", type=int)
-    produtos = get_produtos(categoria_id)
+    produtos = get_produtos()
     return render_template("produto.html", produtos= produtos)
 
-@produto_bp.route("/cadastrar", methods=["POST"])
+@produto_bp.route("/produto/cadastrar", methods=["POST"])
 def adicionar_produto():
     nome = request.form['nome']
     codigo_original = request.form['codigo_original']
-    preco_base = request.form['preco_base']
+    preco_base = float(request.form['preco_base'])
     marca = request.form.get('marca')
     tamanho = request.form.get('tamanho')
     cor = request.form.get('cor')
-    categoria_id = request.form['categoria_id']
+    categoria_id = int(request.form['categoria_id'])
+    quantidade_inicial = int(request.form.get('quantidade_inicial', 0))
+    data_cadastro = request.form.get('data_cadastro') or None
 
-    dados = {
-        "nome": nome,
-        "codigo_original": codigo_original,
-        "preco_base": preco_base,
-        "marca": marca,
-        "tamanho": tamanho,
-        "cor": cor,
-        "categoria_id": categoria_id
-    }
+    insert_produtos(nome, codigo_original, preco_base, marca, tamanho, cor, data_cadastro, categoria_id, quantidade_inicial)
 
-    insert_produtos(dados)
     return redirect(url_for('produto.listar_produtos'))
 
 
-@produto_bp.route("/api/produtos/<int:produto_id>", methods=["PUT"])
-def atualizar_produto(produto_id):
-    try: 
-        dados = request.get_json()
-        if not dados:
-            return jsonify({"erro": "JSON inválido ou vazio"}), 400
-        
-        existente = get_produtos_id(produto_id)
-        if not existente:
-            return jsonify({"erro": "Produto não encontrado"}), 404
-        
-        update_produto(produto_id, dados)
-        return jsonify({"mensagem": "Produto atualizado com sucesso!"})
-    except Exception as err:
-        return jsonify({"erro": str(err)}), 400
-    
+@produto_bp.route("/produto/alterar", methods=["POST"])
+def atualizar_produto():
+    id = int(request.form['id'])
+    nome = request.form['nome']
+    codigo_original = request.form['codigo_original']
+    preco_base = float(request.form['preco_base'])
+    marca = request.form.get('marca')
+    tamanho = request.form.get('tamanho')
+    cor = request.form.get('cor')
+    categoria_id = int(request.form['categoria_id'])
+    quantidade_inicial = int(request.form.get('quantidade_inicial', 0))
+    data_cadastro = request.form.get('data_cadastro') or None
 
-@produto_bp.route("/api/produtos/<int:produto_id>", methods=["DELETE"])
-def inativar_produto(produto_id):
-    try:
-        inative_produto(produto_id)
-        return jsonify({"mensagem": "Produto inativado com sucesso!"}), 200
-    except Exception as err:
-        return jsonify({"erro": str(err)}), 400
+    alterar(id, nome, codigo_original, preco_base, marca, tamanho, cor, categoria_id, quantidade_inicial, data_cadastro)
+    return redirect(url_for('produto.listar_produtos'))
+
+@produto_bp.route("/produto/inativar", methods=["GET"])
+def inativar_produto():
+    id = request.args.get('id')
+
+    inativar(id)
+    return redirect(url_for('produto.listar_produtos'))
     
-@produto_bp.route("/api/produtos/reativar/<int:produto_id>", methods=["PUT"])
-def reativar_produto(produto_id):
-    try:
-        reative_produto(produto_id)
-        return jsonify({"mensagem": "Produto reativado com sucesso!"}), 200
-    except Exception as e:
-        return jsonify({"erro": str(e)}), 400
+@produto_bp.route("/produto/reativar", methods=["GET"])
+def reativar_produto():
+    id = request.args.get('id')
+
+    reativar(id)
+    return redirect(url_for('produto.listar_produtos'))
--- app/models/produto.py
+++ app/models/produto.py
@@ -1,125 +1,62 @@
 from app.models import conexaoBD
 from datetime import date
 
-def get_produtos(categoria_id=None):
+def get_produtos():
     conexao = conexaoBD()
     try: 
         cursor = conexao.cursor(dictionary=True)
         sql = """
             SELECT p.id, p.nome, c.id AS categoria_id, c.nome AS categoria, p.codigo_original, p.preco_base, p.marca, p.tamanho, p.cor, p.data_cadastro, e.quantidade
             FROM produto p
-            JOIN estoque e ON p.id = e.produto_id
             JOIN categoria c ON p.categoria_id = c.id
-            WHERE p.ativo = TRUE
+            JOIN estoque e ON p.id = e.produto_id
+            ORDER BY p.nome
         """
-
-        parametro = []
-
-        if categoria_id: 
-            sql += " AND p.categoria_id = %s"
-            parametro.append(categoria_id)
         
-        cursor.execute(sql, tuple(parametro))
+        cursor.execute(sql)
         produtos = cursor.fetchall()
     finally:
         cursor.close()
         conexao.close()
     return produtos
 
-def get_produtos_inativos(categoria_id=None):
-    conexao = conexaoBD()
-    try: 
-        cursor = conexao.cursor(dictionary=True)
-        sql = """
-            SELECT p.id, p.nome, c.id AS categoria_id, c.nome AS categoria, 
-            p.codigo_original, p.preco_base, p.marca, p.tamanho, p.cor, 
-            p.data_cadastro, e.quantidade
-            FROM produto p
-            JOIN estoque e ON p.id = e.produto_id
-            JOIN categoria c ON p.categoria_id = c.id
-            WHERE p.ativo = FALSE
-        """
-
-        parametro = []
-
-        if categoria_id: 
-            sql += " AND p.categoria_id = %s"
-            parametro.append(categoria_id)
-        
-        cursor.execute(sql, tuple(parametro))
-        produtos = cursor.fetchall()
-    finally:
-        cursor.close()
-        conexao.close()
-    return produtos
-
-def get_produtos_id(produto_id):
+def get_produtos_id(id):
     conexao = conexaoBD()
     try:
         cursor = conexao.cursor(dictionary=True)
         sql = """
-            SELECT p.id, p.nome, c.id AS categoria_id, c.nome AS categoria, 
-            p.codigo_original, p.preco_base, p.marca, p.tamanho, p.cor, 
-            p.data_cadastro, e.quantidade
+            SELECT p.id, p.nome, p.codigo_original, p.preco_base, p.marca, 
+                p.tamanho, p.cor, p.data_cadastro, p.ativo, c.id AS categoria_id, c.nome AS categoria, e.quantidade
             FROM produto p
-            JOIN estoque e ON p.id = e.produto_id
             JOIN categoria c ON p.categoria_id = c.id
-            WHERE p.id = %s AND p.ativo = TRUE
+            JOIN estoque e ON p.id = e.produto_id
+            WHERE p.id = %s
         """
 
-        cursor.execute(sql, (produto_id,))
+        cursor.execute(sql, (id,))
         produto = cursor.fetchone()
     finally:
         cursor.close()
         conexao.close()
     return produto
 
-def get_categorias():
-    conexao = conexaoBD()
-    try:
-        cursor = conexao.cursor(dictionary=True)
-        cursor.execute("SELECT id, nome, descricao FROM categoria ORDER BY nome")
-        categorias = cursor.fetchall()
-    finally:
-        cursor.close()
-        conexao.close()
-    return categorias
-
-def insert_categoria(nome, descricao=None):
-    conexao = conexaoBD()
-    try:
-        cursor = conexao.cursor()
-        sql = "INSERT INTO categoria (nome, descricao) VALUES (%s, %s)"
-        cursor.execute(sql, (nome, descricao))
-        conexao.commit()
-        return cursor.lastrowid
-    finally:
-        cursor.close()
-        conexao.close()
-
-def insert_produtos(dados: dict):
+def insert_produtos(nome, codigo_original, preco_base, marca, tamanho, cor, data_cadastro, categoria_id, quantidade_inicial=0):
     conexao = conexaoBD()
     try: 
         cursor = conexao.cursor()
-        data_cadastro = dados.get('data_cadastro', date.today())
+        if not data_cadastro:
+            data_cadastro = date.today()
 
-        cursor.execute("""
-            INSERT INTO produto 
-            (nome, codigo_original, preco_base, marca, tamanho, cor, data_cadastro, categoria_id)
+        sql = """
+            INSERT INTO produto (nome, codigo_original, preco_base, marca, tamanho, cor, data_cadastro, categoria_id)
             VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
-        """, (
-            dados["nome"],
-            dados["codigo_original"],
-            dados["preco_base"], 
-            dados.get("marca"),
-            dados.get("tamanho"), 
-            dados.get("cor"), 
-            data_cadastro, 
-            dados["categoria_id"]
-        ))
+        """
 
+        cursor.execute(sql, (nome, codigo_original, preco_base, marca, tamanho, cor, data_cadastro, categoria_id))
         produto_id = cursor.lastrowid
-        cursor.execute("INSERT INTO estoque (produto_id, quantidade) VALUES (%s, %s)", (produto_id, dados.get('quantidade_inicial', 0)))
+
+        sql_estoque = "INSERT INTO estoque (produto_id, quantidade) VALUES (%s, %s)"
+        cursor.execute(sql_estoque, (produto_id, quantidade_inicial))
         
         conexao.commit()
         return produto_id
@@ -128,60 +65,48 @@ def insert_produtos(dados: dict):
         conexao.close()
 
 
-def update_produto(produto_id, dados: dict):
+def alterar(id, novoNome, novoCodigoOriginal, novoPrecoBase, novaMarca, novoTamanho, novaCor, novaCategoria, novaQuantidade, novoStatus):
     conexao = conexaoBD()
     try:
         cursor = conexao.cursor()
-        campos = []
-        valores = []
+        sql = """
+            UPDATE produto
+            SET nome = %s, codigo_original = %s, preco_base = %s, marca = %s, tamanho = %s, 
+                cor = %s, categoria_id = %s, ativo = %s
+            WHERE id = %s
+        """
+        cursor.execute(sql, (novoNome, novoCodigoOriginal, novoPrecoBase, novaMarca, novoTamanho, novaCor, novaCategoria, novoStatus, id))
 
-        mapa = {
-            "nome": "nome",
-            "tipo": "tipo",
-            "codigo_original": "codigo_original",
-            "preco_base": "preco_base",
-            "marca": "marca",
-            "tamanho": "tamanho",
-            "cor": "cor",
-            "data_cadastro": "data_cadastro"
-        }
-
-        for chave, coluna in mapa.items():
-            if chave in dados:
-                campos.append(f"{coluna}=%s")
-                valores.append(dados[chave])
-
-        if campos:
-            sql = f"UPDATE produto SET {', '.join(campos)} WHERE id=%s"
-            valores.append(produto_id)
-            cursor.execute(sql, valores)
-    
-        #atualiza o estoque se for preenchido
-        if 'quantidade' in dados and dados['quantidade'] not in [None, ""]:
-            cursor.execute("UPDATE estoque SET quantidade=%s WHERE produto_id=%s", (dados['quantidade'], produto_id))
+        sql_estoque = "UPDATE estoque SET quantidade = %s WHERE produto_id = %s"
+        cursor.execute(sql_estoque, (novaQuantidade, id))
 
         conexao.commit()
+        return cursor.lastrowid
     finally:
         cursor.close()
         conexao.close()
 
 
-def inative_produto(produto_id):
+def inativar(id):
     conexao = conexaoBD()
     try:
         cursor = conexao.cursor()
-        cursor.execute("UPDATE produto SET ativo = FALSE WHERE id = %s", (produto_id,))
+        sql = "UPDATE produto SET ativo = FALSE WHERE id = %s"
+        cursor.execute(sql, (id,))
         conexao.commit()
+        return cursor.lastrowid
     finally:
         cursor.close()
         conexao.close()
 
-def reative_produto(produto_id):
+def reativar(id):
     conexao = conexaoBD()
     try:
         cursor = conexao.cursor()
-        cursor.execute("UPDATE produto SET ativo = TRUE WHERE id = %s", (produto_id,))
+        sql = "UPDATE produto SET ativo = TRUE WHERE id = %s"
+        cursor.execute(sql, (id,))
         conexao.commit()
+        return cursor.lastrowid
     finally:
         cursor.close()
         conexao.close()
