2b017dd01b2e58b4d59ca9cb9b7f8cc242b39fd1
yasmin <yasmimlarissa004@gmail.com>
2025-10-02 19:09:43 -0300

    Nova tela de compras

--- app/controllers/compra_controller.py
+++ app/controllers/compra_controller.py
@@ -1,37 +1,11 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint,render_template
 from app.models.compra import *
 
 compra_bp = Blueprint("compra", __name__)
 
-@compra_bp.route("/compra", methods=["GET"])
-def listar_compra():
-        compra = get_compra()
-        return render_template('compra.html', compra= compra)
-
-@compra_bp.route("/cadastrar", methods=["POST"])
-def cadastrar_compra(dados):
-    produto_id = request.form['produto_id']
-    nome_produto = request.form['nome_produto']
-    fornecedor_id = request.form['fornecedor_id']
-    nome_fornecedor = request.form['nome_fornecedor']
-    quantidade = request.form['quantidade']
-    preco_compra = request.form['preco_compra']
-    data_compra = request.form['data_compra']
-    categoria = request.form['categoria']
-
-    dados = {
-        "produto_id": produto_id,
-        "nome_produto": nome_produto,
-        "fornecedor_id": fornecedor_id,
-        "nome_fornecedor": nome_fornecedor,
-        "quantidade": quantidade,
-        "preco_compra": preco_compra,
-        "data_compra": data_compra,
-        "categoria": categoria
-    }
-
-
-    insert_compras(dados)
-    return redirect(url_for('compra.listar_compra'))
-
+@compra_bp.route("/produto/<int:produto_id>/compras")
+def compras_por_produto(produto_id):
+    compras = compras.query.filter_by(produto_id  = produto_id).all()
+     
+    return render_template("compras.html", compras = compras)
  
--- app/models/compra.py
+++ app/models/compra.py
@@ -1,91 +1,45 @@
 from app.models import conexaoBD
-from datetime import date
 
-def get_compra():
+def listar_compras_por_produto(produto_id):
     conexao = conexaoBD()
-    try:
-        cursor = conexao.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM compra")
-        compras = cursor.fetchall()
-    finally:
-        cursor.close()
-        conexao.close()
+    cursor = conexao.cursor(dictionary=True)
+
+    query = """
+        SELECT id, produto_id, quantidade, valor_total, data
+        FROM compras
+        WHERE produto_id = %s
+    """
+    cursor.execute(query, (produto_id,))
+    compras = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
     return compras
 
-
-def get_compra_id(compra_id):
+# Inserir nova compra
+def inserir_compra(produto_id, quantidade, valor_total):
     conexao = conexaoBD()
-    try:
-        cursor = conexao.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM compra WHERE id = %s", (compra_id,))
-        compra = cursor.fetchone()
-    finally:
-        cursor.close()
-        conexao.close()
-    return compra
+    cursor = conexao.cursor()
 
+    query = """
+        INSERT INTO compras (produto_id, quantidade, valor_total)
+        VALUES (%s, %s, %s)
+    """
+    cursor.execute(query, (produto_id, quantidade, valor_total))
+    conexao.commit()
 
-def insert_compras(dados: dict):
+    cursor.close()
+    conexao.close()
+
+# Buscar uma compra pelo ID
+def buscar_compra_por_id(compra_id):
     conexao = conexaoBD()
-    try:
-        cursor = conexao.cursor()
-        data_compra = dados.get('data_compra', date.today())
-        
-        cursor.execute("""
-            INSERT INTO compra
-            (produto_id, quantidade, preco_compra, cliente_id, data_compra)
-            VALUES (%s, %s, %s, %s, %s)
-        """, (
-            dados['produto_id'],
-            dados['quantidade'],
-            dados['preco_compra'],
-            dados.get('fornecedor_id'),
-            data_compra
-        ))
-        
-        compra_id = cursor.lastrowid
+    cursor = conexaoBD.cursor(dictionary=True)
 
-        cursor.execute("""
-            UPDATE estoque
-            SET quantidade = quantidade + %s
-            WHERE produto_id = %s
-        """, (dados['quantidade'], dados['produto_id']))
+    query = "SELECT id, produto_id, quantidade, valor_total, data FROM compras WHERE id = %s"
+    cursor.execute(query, (compra_id,))
+    compra = cursor.fetchone()
 
-        conexao.commit()
-        return compra_id
-    finally:
-        cursor.close()
-        conexao.close()
-
-def update_compra(compra_id, dados: dict):
-    conexao = conexaoBD()
-    try:
-        cursor = conexao.cursor()
-        campos = []
-        valores = []
-
-        mapa = {
-            "nome": "nome",
-            "tipo": "tipo",
-            "codigo_original": "codigo_original",
-            "preco_base": "preco_base",
-            "marca": "marca",
-            "tamanho": "tamanho",
-            "cor": "cor",
-            "data_cadastro": "data_cadastro"
-        }
-
-        for chave, coluna in mapa.items():
-            if chave in dados:
-                campos.append(f"{coluna}=%s")
-                valores.append(dados[chave])
-
-        if campos:
-            sql = f"UPDATE produto SET {', '.join(campos)} WHERE id=%s"
-            valores.append(compra_id)
-            cursor.execute(sql, valores)
-
-        conexao.commit()
-    finally:
-        cursor.close()
-        conexao.close()
+    cursor.close()
+    conexao.close()
