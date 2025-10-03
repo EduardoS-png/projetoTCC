3d5f14f62158381f55569411d939dd860dace422
EdS <Sousaa.dev@gmail.com.br>
2025-10-03 16:20:45 -0300

    Alteração em pequenas coisas no controller e no compra.html

--- app/controllers/inventario_controller.py
+++ app/controllers/inventario_controller.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
 from app.models.inventario import get_inventario
+from app.models.fornecedor import get_fornecedores
 
 inventario_bp = Blueprint("inventario", __name__)
 
 @inventario_bp.route("/inventario/lista", methods=["GET"])
 def listar_inventario():
     produtos = get_inventario()
-    return render_template("inventario.html", produtos=produtos)
+    fornecedores = get_fornecedores()
+    return render_template("inventario.html", produtos=produtos, fornecedores= fornecedores)
